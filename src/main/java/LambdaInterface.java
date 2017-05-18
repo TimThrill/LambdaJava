5aa14dcfbf91fbe577535b034fbc11b1f4da567c
cong <macong1989@gmail.com>
2017-05-18 11:37:05 +1000

    Add lambda explanation

--- src/main/java/LambdaInterface.java
+++ src/main/java/LambdaInterface.java
@@ -1,5 +1,10 @@
 /**
  * Created by cong on 17/05/17.
+ *
+ * Lambda expression can be used in a functional interface
+ * The term "functional interface" is an interface with only one abstract method
+ * In fact, conversion to a functional interface is the only thing you can do with
+ * a lambda expression in Java.
  */
 public interface LambdaInterface {
     void printLambda();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,16 @@
 public class Main {
     public static void main(String[] args) {
         Task task = new Task();
+
+        // This is an anonymous class
+        task.doTask(new LambdaInterface() {
+            @Override
+            public void printLambda() {
+                System.out.println("This is anonymous print lambda");
+            }
+        });
+
+        // This is the lambda expression
         task.doTask(() -> System.out.println("This is lambda"));
     }
 }
